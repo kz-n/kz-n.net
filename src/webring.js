026767e9a89fa8966667cb88f9ad7e4f0c5616ba
kz-n <64893077+kz-n@users.noreply.github.com>
2023-09-06 12:44:56 +0200

    wake up babe webring is added

---
+++ src/webring.js
@@ -0,0 +1,9 @@
+export const webring = {
+    '/88by31s/armcord.gif': "https://armcord.app",
+    '/88by31s/birb.gif': "https://birb.works",
+    '/88by31s/cdn.gif': "https://cdn.fl1nt.dev",
+    '/88by31s/coinpilot.gif': "https://coinpilot.com",
+    '/88by31s/flint.gif': "https://fl1nt.dev",
+    '/88by31s/thebee.gif': "https://thebeemc.carrd.co/",
+    '/88by31s/zeroxbit.gif': "https://goodrich.dev"
+}
