0f0ddf186f94b6f5b1891cf0a941b4ee509710fe
kz-n <64893077+kz-n@users.noreply.github.com>
2024-02-27 14:00:35 +0100

    austin texas

--- src/webring.js
+++ src/webring.js
@@ -4,6 +4,5 @@ export const webring = {
     '/88by31s/cdn.gif': "https://cdn.fl1nt.dev",
     '/88by31s/coinpilot.gif': "https://coinpilot.com",
     '/88by31s/flint.gif': "https://fl1nt.dev",
-    '/88by31s/thebee.gif': "https://thebeemc.carrd.co/",
-    '/88by31s/zeroxbit.gif': "https://goodrich.dev"
+    '/88by31s/thebee.gif': "https://thebeemc.carrd.co/"
 }
