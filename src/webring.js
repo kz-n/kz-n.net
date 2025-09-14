c05476b15e47397de8f3c14ec3e2017d9df9042b
kz-n <64893077+kz-n@users.noreply.github.com>
2025-09-14 12:24:31 +0200

    sex

--- src/webring.js
+++ src/webring.js
@@ -1,8 +1,6 @@
 export const webring = {
-    '/88by31s/armcord.gif': "https://armcord.app",
-    '/88by31s/birb.gif': "https://birb.works",
+
     '/88by31s/cdn.gif': "https://cdn.fl1nt.dev",
-    '/88by31s/coinpilot.gif': "https://coinpilot.com",
     '/88by31s/flint.gif': "https://fl1nt.dev",
     '/88by31s/thebee.gif': "https://thebeemc.carrd.co/"
 }
